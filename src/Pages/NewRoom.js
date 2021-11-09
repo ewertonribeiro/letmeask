2850a0992922767891086dd67a1d07f9110d71f0
ewertonribeiro <ewertondosanjosz@gmail.com>
2021-11-09 16:08:08 -0300

    Sala de perguntas admin

--- src/Pages/NewRoom.js
+++ src/Pages/NewRoom.js
@@ -1,14 +1,41 @@
 import Logo from '../Assets/images/Logo.png'
-
+import Copy from '../Assets/icons/copy.png'
+import Asks from '../Assets/images/Ilustração.png'
+import '../Styles/Room.scss'
 export function NewRoom(){
 
     return(
-       
+       <div className='NewRoom'>
         <header>
-            <div>
+            <div className='logo'>
                 <img src={Logo} alt='logo'/>
             </div>
+            <div className='Buttons'>
+               
+              
+               <button className='First'>
+                <div>
+                   <img src={Copy} alt='copy'/>
+                   </div>
+                <span>Sala # 232425</span>
+                </button>
+                
+                <button className='Second'>Encerrar Sala!</button>
+                </div>
+        
         </header>
-       
+        
+
+        <section>
+            <main>
+            <div className="Title"><h1>Sala React Q&A</h1></div>
+            <div className='img'>
+                <img src={Asks}alt='Perguntas'/>
+                <h2>Nenhuma Pergunta por aqui....</h2>
+                <p>Envie o código desta sala para os seus amigos e comece a responder perguntas!!</p>
+            </div>
+            </main>
+        </section>
+        </div>
     )
 }
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,7 +1,7 @@
 import {BrowserRouter , Switch , Route} from 'react-router-dom'
 import {CreateNewRoom} from '../Pages/CreateNewRoom'
 import { Home } from '../Pages/Home';
-
+import {NewRoom} from '../Pages/NewRoom'
 
 
 export function Routess(){
@@ -10,6 +10,7 @@ export function Routess(){
          <Switch>
              <Route component={Home} exact path="/"/>
              <Route component={CreateNewRoom} path='/CreateRoom'/>
+             <Route component={NewRoom} path='/NewRoom'/>
          </Switch>
      </BrowserRouter>
             
